da194b033d93c3b1ffc4e0e867a826a2b2db8977
restudent <adi.moser@gmail.com>
2022-05-17 12:12:20 +0000

    change

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Requirements Engineering Friend Tracker (mosa)",
             "formList": [
                 {
                     "id": "FriendForm",
